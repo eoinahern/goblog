9dfc6885659aee8dcb27af57d2423d832b8b88d4
eoinahern <eoin.ahern@mycit.ie>
2018-09-06 18:39:35 +0100

    testing docker swarm

--- copyall.sh
+++ copyall.sh
@@ -5,3 +5,8 @@ export CGO_ENABLED=0
 cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
 
 export GOOS=darwin
+
+docker build -t eoinahern/accountservice accountservice/
+
+docker service rm accountservice
+docker service create --name=accountservice --replicas=1 --network=my_network -p=8080:8080 eoinahern/accountservice
